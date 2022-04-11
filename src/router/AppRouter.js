919ed8044ec209dd1f0158990b6c851dc24cbfec
Daniel Gorianz <aalbaafikaa@gmail.com>
2022-04-11 15:05:52 -0400

    diseño login hecho

---
+++ src/router/AppRouter.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { LoginScreen } from '../components/auth/LoginScreen'
+import { CalendarScreen } from '../components/calendar/CalendarScreen'
+
+
+export const AppRouter = () => {
+  return (
+    <Router>
+        <Routes>
+            <Route path='/login' element={ <LoginScreen /> }/>
+            <Route path='/*' element={ <CalendarScreen /> } />
+        </Routes>
+    </Router>
+  )
+}
